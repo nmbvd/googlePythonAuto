5f25a1e67c04539fbad6d1c4474727a62ef3453f
nmbvd <guojia1945@sina.com>
2020-11-15 15:14:55 +0800

    create a lessonScripts folder to save scripts

---
+++ lessonScripts/all_check.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+imort is
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exist("/run/reboot-required")
+def main():
+	pass
+main()
